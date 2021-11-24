0f42acd7e297efe4567dda09903b61416a04978d
Szabó Péter <36503704+petya712@users.noreply.github.com>
2021-11-24 01:38:18 +0100

    Update DOMQuerryID4JQM.java

--- XMLTaskID4JQM/DOMParseID4JQM/src/hu/domparse/ID4JQM/DOMQuerryID4JQM.java
+++ XMLTaskID4JQM/DOMParseID4JQM/src/hu/domparse/ID4JQM/DOMQuerryID4JQM.java
@@ -19,27 +19,28 @@ public class DOMQuerryID4JQM {
 
 	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, TransformerException  {
 		// TODO Auto-generated method stub
-		File xmlFile = new File("XMLID4JQM.xml"); //xml fájl bekérése
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); //olvasás lehetővé tétele
+		File xmlFile = new File("XMLID4JQM.xml"); //Xml fájl bekérése
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); //Olvasás lehetővé tétele
         DocumentBuilder dBuilder = factory.newDocumentBuilder();
         Document doc = dBuilder.parse(xmlFile);
         doc.getDocumentElement().normalize();
 
 
-        System.out.println("Gyökér elem: " + doc.getDocumentElement().getNodeName());
+        System.out.println("Gyökér elem: " + doc.getDocumentElement().getNodeName());//Gyökér elem kiiratása
         System.out.println("------------------------------");
         LoadQuery(doc);
 
 	}
 	
 	public static void LoadQuery(Document doc) throws TransformerException {
-		NodeList nodeList = doc.getElementsByTagName("diak"); // Diákok listázása
+		NodeList diaknodeList = doc.getElementsByTagName("diak"); // Diákok listája
+		NodeList osztalynList = doc.getElementsByTagName("osztaly");//Osztályok listája
 		
 		Element element = null;
 		Node nNode = null;
-		
-		for (int i = 0; i < nodeList.getLength(); i++) {
-			nNode = nodeList.item(i);
+		//Diákok ID és osztály ID szerinti kilistázása, hogy később tudjunk választani diákot lekérdezésre
+		for (int i = 0; i < diaknodeList.getLength(); i++) {
+			nNode = diaknodeList.item(i);
 			element = (Element) nNode;
 			String id =  element.getAttribute("id");
 			String osztid =  element.getAttribute("osztalyid");
@@ -55,15 +56,15 @@ public class DOMQuerryID4JQM {
 		String diakid = in.next();
 		
 		
-    	for (int i = 0; i < nodeList.getLength(); i++) {
-    		nNode = nodeList.item(i);
+    	for (int i = 0; i < diaknodeList.getLength(); i++) {
+    		nNode = diaknodeList.item(i);
     		element = (Element) nNode;
     		int db = element.getElementsByTagName("vezeteknev").getLength();
     		if(nNode.getNodeType() == Node.ELEMENT_NODE) { // Ha megfelelő ID-t adtunk meg akkor elindítja a lekérdező metódust (QueryXML)
   
     			
     			if(element.getAttribute("id").equals(diakid)) {
-    				QueryXML(db,doc,element);
+    				QueryXML(db,doc,element,osztalynList);
 
     			}
     		}
@@ -71,23 +72,30 @@ public class DOMQuerryID4JQM {
     	in.close();
 		
 	}
-	// Kiválasztott diák adatainak kiíratása
-	public static void QueryXML(int db,Document doc, Element element) {
+	// A lekérdezett diák adatainak kiíratási metódusa
+	public static void QueryXML(int db,Document doc, Element element, NodeList osztalynList) {
+		Node nNode = null;
+		Element elem = null;
 		String id = element.getAttribute("id");
 		
 		System.out.println("\nDiák adatai:\n");
 		System.out.println("Diák ID: " + id);
 		for (int i = 0; i < db; i++) {
 			
+			nNode = osztalynList.item(i); //Az átadott osztály nodelistából kiválasztjuk az i. elemet
+			elem = (Element) nNode;
+			
+			//Stringbe szedett kiiratása az elemeknek és attributumoknak a gyökér elemből (iskola) és a diák és osztály elemből
 			String diakcim =  element.getElementsByTagName("cim").item(i).getTextContent();
 			String diaknem =  element.getElementsByTagName("nem").item(i).getTextContent();
 			String osztid =  element.getAttribute("osztalyid");
+			String osztalynev =  elem.getElementsByTagName("nev").item(i).getTextContent();
 			String szulinap =  element.getElementsByTagName("szulinap").item(i).getTextContent();
 			String vname =  element.getElementsByTagName("vezeteknev").item(i).getTextContent();
 			String kname = element.getElementsByTagName("keresztnev").item(i).getTextContent();
 			String iskolaneve = doc.getElementsByTagName("nev").item(i).getTextContent();
 
-			System.out.println("Iskola neve: "+iskolaneve+"\nOsztálya: "+osztid+"\nVezetékneve: "+vname+"\nKeresztneve: "+kname+"\nSzületési ideje: "+szulinap+"\nNeme: "+diaknem+"\nLakcíme: "+diakcim);
+			System.out.println("Iskola neve: "+iskolaneve+"\nOsztályának ID-je: "+osztid+"\nOsztályának neve "+osztalynev+"\nVezetékneve: "+vname+"\nKeresztneve: "+kname+"\nSzületési ideje: "+szulinap+"\nNeme: "+diaknem+"\nLakcíme: "+diakcim);
 		}
 	}
 
